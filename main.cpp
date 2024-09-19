a5c53c92e46271492c768eab00e4455ae224f3fe
ofidohub <ofidohub@gmail.com>
2024-09-19 07:32:56 +0200

    commit "add cmake files, build directory and main.cpp"

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+// #include <Arduino.h>
+// #include "sensors/soil_moisture_sensor.h"
+// #include "sensors/temperature_sensor.h"
+// #include "actuators/irrigation_system.h"
+// #include "actuators/robotic_arm.h"
+// #include "utils/mqtt_client.h"
+// #include "utils/http_client.h"
+
+/// void setup()
+
+// Initialize sensors and actuators
+// initSoilMoistureSensor();
+//  initTemperatureSensor();
+//  initIrrigationSystem();
+//  initRoboticArm();
+
+// Initialize communication
+// initMQTTClient();
+// initHTTPClient();
+//}
+
+////void loop()
+//{
+// Main logic for your project
+// monitorSoilMoisture();
+// monitorTemperature();
+// controlIrrigationSystem();
+//  controlRoboticArm();
+// sendDataToCloud();
+//}
